38cf64675ba9b8d87c2203c232eb0e133562eb2f
Todor Pavlov <pavlovt@gmail.com>
2018-06-22 11:55:04 +0300

    seats editor update

--- src/pages/drag.vue
+++ src/pages/drag.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="drag">
     <div style="width: 10%; float: left;">
-      <img src="https://svgshare.com/i/7BU.svg" alt="" width="50px" class="drag">
+      <img id="drag1" src="https://svgshare.com/i/7BU.svg" alt="" width="50px" class="drag">
     </div>
-    <div style="width: 90%; float: left;" class="frame">
-      <img src="https://cdn.pbrd.co/images/HqV80K4.jpg" alt="" width="100%">
+    <div style="width: 90%; height: 90vh; float: left; background-image: url('https://cdn.pbrd.co/images/HqV80K4.jpg'); background-repeat: no-repeat; background-size: 100%;" class="frame">
+      <!-- <img src="https://cdn.pbrd.co/images/HqV80K4.jpg" alt="" width="100%"> -->
     </div>
   </div>
 </template>
@@ -29,7 +29,7 @@
             containment: 'frame',
 
             //When first dragged
-            stop:function(ev, ui) {
+            /*stop:function(ev, ui) {
               var pos=$(ui.helper).offset();
               let objName = "#clonediv"+counter
               $(objName).css({"left":pos.left,"top":pos.top});
@@ -46,7 +46,7 @@
                       console.log(pos.top)
                     }
                 });
-            }
+            }*/
         });
         //Make element droppable
         $(".frame").droppable({
@@ -55,10 +55,12 @@
               counter++;
               var element=$(ui.draggable).clone();
               element.addClass("tempclass");
+              console.log(ui.helper.attr('style'))
+              element.attr('style', ui.helper.attr('style'))
               $(this).append(element);
               $(".tempclass").attr("id","clonediv"+counter);
               $("#clonediv"+counter).removeClass("tempclass");
-
+ui.helper.remove()
               //Get the dynamically item id
               // draggedNumber = ui.helper.attr('id').search(/drag([0-9])/)
               // itemDragged = "dragged" + RegExp.$1
